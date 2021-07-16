478485b8252b127e5e8af827a18ef47972de270c
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-07-15 21:16:36 -0400

    Adding a basic file to solve the problem set

---
+++ cp/contest_solve.py
@@ -0,0 +1,33 @@
+import sys,math,cmath,time,collections
+start_time = time.time()
+
+
+def solve(a, b):
+	# The actual solution begins here
+	c = a + b
+	print(c)
+
+def main():
+	# Main function dedicated to get the I/P
+	a, b = map(int, input().split())
+	solve(a, b)
+
+
+if __name__ == '__main__':
+	# ONLINE_JUDGE = __debug__
+	ONLINE_JUDGE = False
+	print(ONLINE_JUDGE)
+	
+	# If it's Local - Get I/P from file
+	if not ONLINE_JUDGE:
+		sys.stdin = open('data.in', "r")
+		sys.stdout = open('data.out', "w")
+
+	testcases = int(input())
+	for i in range(testcases):
+		main()
+
+	# If it's local - Print this O/P
+	if not ONLINE_JUDGE:
+		print(f"Time Elapsed: {time.time() - start_time} seconds")
+		sys.stdout.close()
