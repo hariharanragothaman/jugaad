f91f5ad7559fd7b39a15a56bd0ee1ce3e8dbe6b6
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-07-18 18:02:02 -0400

    Adding proper python template - used in Jetbranis

--- cp/contest_solve.py
+++ cp/contest_solve.py
@@ -1,4 +1,4 @@
-import sys,math,cmath,time,collections
+import sys, math, cmath, time, collections
 from collections import deque, Counter, OrderedDict
 from heapq import nsmallest, nlargest, heapify, heappop, heappush, heapreplace
 from math import ceil, floor, log, log2, sqrt, gcd, factorial, pow, pi
@@ -8,37 +8,38 @@ start_time = time.time()
 
 
 def solve(a, b):
-	"""
-	The actual solution begins here
-	c = a + b
-	print(c)
-	"""
-	pass
+    """
+    The actual solution begins here
+    c = a + b
+    print(c)
+    """
+    pass
 
 
 def main():
-	"""
-	Main function dedicated to get the I/P
-	a, b = map(int, input().split())
-	solve(a, b)
-	"""
-	pass
+    """
+    Main function dedicated to get the I/P
+    a, b = map(int, input().split())
+    solve(a, b)
+    """
+    a, b = map(int, input().split())
+    solve(a, b)
 
-if __name__ == '__main__':
-	# ONLINE_JUDGE = __debug__
-	ONLINE_JUDGE = False
-	print(ONLINE_JUDGE)
-	
-	# If it's Local - Get I/P from file
-	if not ONLINE_JUDGE:
-		sys.stdin = open('data.in', "r")
-		sys.stdout = open('data.out', "w")
 
-	testcases = int(input())
-	for i in range(testcases):
-		main()
+if __name__ == "__main__":
+    # ONLINE_JUDGE = __debug__
+    ONLINE_JUDGE = False
 
-	# If it's local - Print this O/P
-	if not ONLINE_JUDGE:
-		print(f"Time Elapsed: {time.time() - start_time} seconds")
-		sys.stdout.close()
+    # If it's Local - Get I/P from file
+    if not ONLINE_JUDGE:
+        sys.stdin = open("data.in", "r")
+        sys.stdout = open("data.out", "w")
+
+    testcases = int(input())
+    for i in range(testcases):
+        main()
+
+    # If it's local - Print this O/P
+    if not ONLINE_JUDGE:
+        print(f"Time Elapsed: {time.time() - start_time} seconds")
+        sys.stdout.close()
