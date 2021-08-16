5eeb5f14fade77970f357de306735ec218d36e96
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-08-15 22:05:12 -0400

    Updating the contest template

--- cp/contest.cpp
+++ cp/contest.cpp
@@ -1,5 +1,18 @@
-/* BEGIN HEADER FILES AND OTHER OPTIZATIONS */
+/*
+    Some General Advice
+    REMEMBER CLEAR GLOBAL STATE
+    REMEMBER READ THE PROBLEM STATEMENT AND DON'T SOLVE A DIFFERENT PROBLEM
+    remember hidden T factor of 1e2
+    read the bounds for stupid cases
+    did you restart your editor
+    pushing back vectors is garbage, pre-initialize them
+    function calls are not free
+*/
+
+
 #pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("inline")
 
 #include <iostream>
 #include <fstream>
@@ -53,12 +66,77 @@ typedef pair<ll, ll> pll;
 #define lb lower_bound
 #define ub upper_bound
 
+#define debug(x) cout << #x << " is " << x << endl
 
-// begin deepsigh
+vector<int> sieve(int n)
+{
+    int*arr = new int[n + 1]();
+    vector<int> vect;
+    for (int i = 2; i <= n; i++)
+        if (arr[i] == 0)
+        {
+            vect.push_back(i);
+            for (int j = 2 * i; j <= n; j += i)arr[j] = 1;
+        }
+    return vect;
+}
+ll gcd(ll a, ll b) {
+    if (b > a) {
+        return gcd(b, a);
+    }
+    if (b == 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+ll expo(ll a, ll b, ll mod) {
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1)res = (res * a) % mod;
+        a = (a * a) % mod;
+        b = b >> 1;
+    }
+    return res;
+}
+void extendgcd(ll a, ll b, ll*v) {
+    if (b == 0) {
+        v[0] = 1;    //pass an arry of size 3
+        v[1] = 0;
+        v[2] = a;
+        return ;
+    }
+    extendgcd(b, a % b, v);
+    ll x = v[1];
+    v[1] = v[0] - v[1] * (a / b);
+    v[0] = x;
+    return;
+}
+ll mminv(ll a, ll b) {
+    ll arr[3];    //for non prime b
+    extendgcd(a, b, arr);
+    return arr[0];
+}
+ll mminvprime(ll a, ll b) {
+    return expo(a, b - 2, b);
+}
+bool revsort(ll a, ll b) {
+    return a > b;
+}
+void swap(int &x, int &y) {
+    int temp = x;
+    x = y;
+    y = temp;
+}
+ll combination(ll n, ll r, ll m, ll* fact) {
+    ll val1 = fact[n];
+    ll val2 = mminvprime(fact[r], m);
+    ll val3 = mminvprime(fact[n - r], m);
+    return ((val1 * val2) % m * val3) % m;
+}
+
+#define ENABLEFASTIO() std::ios::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr)
 #define min(x,y) ({ __typeof__(x) __var0 = x; __typeof__(y) __var1 = y; __var0 < __var1 ? __var0 : __var1; })
 #define max(x,y) ({ __typeof__(x) __var0 = x; __typeof__(y) __var1 = y; __var0 < __var1 ? __var1 : __var0; })
-#define ENABLEFASTIO() std::ios::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr)
-// end deepsigh
 
 // BEGIN NO SAD
 #define rep(i, a, b) for(int i = a; i < (b); ++i)
@@ -67,38 +145,31 @@ typedef pair<ll, ll> pll;
 #define sz(x) (int)(x).size()
 // END NO SAD
 
-/* END HEADER FILES AND OTHER OPTIZATIONS */
-
-/* 
-Some General Advice 
-
-REMEMBER CLEAR GLOBAL STATE
-REMEMBER READ THE PROBLEM STATEMENT AND DON'T SOLVE A DIFFERENT PROBLEM
-remember hidden T factor of 1e2
-read the bounds for stupid cases
-did you restart your editor
-pushing back vectors is garbage, pre-initialize them
-function calls are not free
-*/
-
-
 
 #define ONLINE_JUDGE   /* IF not ONLINE_JUDGE Comment this line*/
 
 #ifndef ONLINE_JUDGE
-ifstream  i_data("data.in");
-ofstream  o_data("data.out");
+ifstream  i_data("../io/data.in");
+ofstream  o_data("../io/data.out");
 #define cin  i_data
 #define cout o_data
 #else
 #endif
 
+void solve()
+{
+
+}
 
 int main()
 {
-    ENABLEFASTIO();
-	int a, b;
-	cin >> a >> b;
-	cout << a + b << endl;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int T;
+    cin>>T;
+    while(T--) solve();
+    return 0;
 
 }
