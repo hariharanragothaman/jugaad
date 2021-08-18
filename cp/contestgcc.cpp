320b21093aa29353eed0af94d6beb6bac598ab87
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-08-18 19:02:09 -0400

    Template when using linux system

---
+++ cp/contestgcc.cpp
@@ -0,0 +1,78 @@
+/* BEGIN HEADER FILES AND OTHER OPTIZATIONS */
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("inline")
+
+#include "bits/stdc++.h"
+using namespace std;
+
+
+// Some basic typedef's for comfort
+typedef vector<int> vi;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+// #defines for bounds comfort
+#define double long double
+
+// #defines for comfort
+#define f first
+#define s second
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+
+// Binary Search comforts
+#define lb lower_bound
+#define ub upper_bound
+
+
+// begin deepsigh
+#define min(x,y) ({ __typeof__(x) __var0 = x; __typeof__(y) __var1 = y; __var0 < __var1 ? __var0 : __var1; })
+#define max(x,y) ({ __typeof__(x) __var0 = x; __typeof__(y) __var1 = y; __var0 < __var1 ? __var1 : __var0; })
+#define ENABLEFASTIO() std::ios::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr)
+// end deepsigh
+
+// BEGIN NO SAD
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+// END NO SAD
+
+/* END HEADER FILES AND OTHER OPTIZATIONS */
+
+/* 
+Some General Advice 
+
+REMEMBER CLEAR GLOBAL STATE
+REMEMBER READ THE PROBLEM STATEMENT AND DON'T SOLVE A DIFFERENT PROBLEM
+remember hidden T factor of 1e2
+read the bounds for stupid cases
+did you restart your editor
+pushing back vectors is garbage, pre-initialize them
+function calls are not free
+*/
+
+
+
+#define ONLINE_JUDGE   /* IF not ONLINE_JUDGE Comment this line*/
+
+#ifndef ONLINE_JUDGE
+ifstream  i_data("data.in");
+ofstream  o_data("data.out");
+#define cin  i_data
+#define cout o_data
+#else
+#endif
+
+
+int main()
+{
+    ENABLEFASTIO();
+	int a, b;
+	cin >> a >> b;
+	cout << a + b << endl;
+
+}
