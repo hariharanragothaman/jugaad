34c46d02b2d212e0e4f9e1b02cab173e8c216c68
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2022-07-10 19:30:57 -0400

    Updating templates

--- cp/contest.py
+++ cp/contest.py
@@ -17,19 +17,24 @@ from bisect import bisect_left, bisect_right
 from functools import reduce
 
 # SOME GENERAL HELPER
-def input_as_array(): return list(map(int, input().split()))
+def input_as_array():
+    return list(map(int, input().split()))
+
 
 def debug():
     if os.path.exists("data.in"):
         print("*" * 10)
 
+
 def debug3():
     if os.path.exists("data.in"):
         print("-" * 10)
 
+
 def debug2(value):
-    if os.path.exists('data.in'):
+    if os.path.exists("data.in"):
         print(value)
+
 
 start_time = time.time()
 
@@ -37,12 +42,13 @@ start_time = time.time()
 def solve():
     pass
 
+
 def main():
     pass
 
 
 if __name__ == "__main__":
-    if os.path.exists('data.in'):
+    if os.path.exists("data.in"):
         sys.stdin = open("data.in", "r")
         sys.stdout = open("data.out", "w")
 
@@ -51,6 +57,6 @@ if __name__ == "__main__":
         main()
 
     # If it's local - Print this O/P
-    if os.path.exists('data.in'):
+    if os.path.exists("data.in"):
         print(f"Time Elapsed: {time.time() - start_time} seconds")
         sys.stdout.close()
